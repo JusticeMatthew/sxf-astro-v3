8fdb6a99439787ee4aa2a8b3d64c11ab3aa8e4ad
Matthew <matthewajustice@gmail.com>
2024-07-15 21:25:54 -0400

    header finished? added scaffolding for other pages to make sure links worked

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'Fill me in',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
